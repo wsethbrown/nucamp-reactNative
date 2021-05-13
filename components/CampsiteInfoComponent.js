a9abd60452543cad144712ac77ec29427d74f3c7
Seth Brown <seth@pop-os.localdomain>
2021-05-13 12:44:54 -0400

    add navigation and home

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import {Card} from 'react-native-elements';
+import {CAMPSITES} from '../shared/campsites';
+
 
 function RenderCampsite({campsite}) {
     if (campsite) {
@@ -18,8 +20,23 @@ function RenderCampsite({campsite}) {
     return <View />;
 }
 
-function CampsiteInfo(props) {
-    return <RenderCampsite campsite={props.campsite} />;
+class CampsiteInfo extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
+
+    static NavigationOptions = {
+        title: 'Campsite Information'
+    }
+
+    render(){
+        const campsiteId = this.props.navigation.getParam('campsiteId');
+        const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0];
+        return <RenderCampsite campsite={campsite} />;
+    }
 }
 
 export default CampsiteInfo;
