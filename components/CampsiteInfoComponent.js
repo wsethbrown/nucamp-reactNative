b9d5f07066876cd22d40b0c22c8156ba88e5ecb9
Seth <wsethbrown@gmail.com>
2021-06-03 11:40:03 -0400

    add social share

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder} from 'react-native';
+import {Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder, Share} from 'react-native';
 import {Card, Icon, Input} from 'react-native-elements';
 import {Rating} from 'react-native-ratings';
 import {connect} from 'react-redux';
@@ -64,7 +64,13 @@ function RenderCampsite(props) {
     
     const recognizeComment = ({dx}) => (dx > 200) ? true : false
 
-    
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title,
+            message: `${title}: ${message} ${url}`,
+            url
+        })
+    }
 
     if (campsite) {
         return (
@@ -97,6 +103,16 @@ function RenderCampsite(props) {
                             raised
                             reverse
                             onPress={() => props.onShowModal()}
+                        />
+
+                        <Icon
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description,
+                                baseUrl + campsite.image)}
                         />
                     </View>
                 </Card>
